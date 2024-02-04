7f91a0d0d421e04e3c0e182e1a30233367a598de
rilwangiwa <giwarilwan00@gmail.com>
2024-02-04 02:27:56 +0100

    continuos commit

---
+++ controllers/main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const login = (req, res) => {
+  res.send('login');
+};
+
+const dashboard = (req, res) => {
+  const rand = Math.floor(Math.random * 100);
+  res
+    .status(200)
+    .json({
+      msg: `this is a random number`,
+      secret: `the number is from ${rand}`,
+    });
+};
+
+module.exports = { login, dashboard };
