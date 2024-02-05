0b6dd2c83382b3b56709dff0e93dffb8bc0e2f65
rilwangiwa <giwarilwan00@gmail.com>
2024-02-05 03:31:41 +0100

    update

--- controllers/main.js
+++ controllers/main.js
@@ -1,14 +1,25 @@
-const express = require('express');
+const customAPIError = require('../errors/custom-error');
+const jwt = require('jsonwebtoken');
 
 const login = (req, res) => {
-  res.send('login');
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new customAPIError('please provide a username and password', 400);
+  }
+  // db usually provides ID, this is a demo
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+  res.status(200).json({ msg: 'user created', token });
 };
 
 const dashboard = (req, res) => {
   const rand = Math.floor(Math.random() * 100);
   res.status(200).json({
-    msg: `this is a random number`,
-    secret: `the number is ${rand}`,
+    msg: `Hello, ${decoded.username}`,
+    secret: `the number is your number ${rand}`,
   });
 };
 
--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const authenMiddleware = require('../middleware/auth');
 
 const { login, dashboard } = require('../controllers/main');
 
-router.route('/dashboard').get(dashboard);
+router.route('/dashboard').get(authenMiddleware, dashboard);
 router.route('/login').post(login);
 
 module.exports = router;
